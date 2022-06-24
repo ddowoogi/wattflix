7838cb962ee45792340db926e899535f62eb68f1
saehim <saeilee.vxv@gmail.com>
2022-06-25 00:29:06 +0900

    Javascript - bookmark - 1차 완료

--- bookmark.js
+++ bookmark.js
@@ -17,35 +17,29 @@ $(document).ready(function(){
 
     $('.comment').html("<h3>"+$userName+" 님,<br>더 많은 영화를<br>찾아보세요.</h3><br>보고싶어요 표시한 영화 <b>"+$markCount+"</b>개");
 
-    let $movieItem = $('.movieItem');
+    // 북마크 팝업
+    let $moviePoster = $('.moviePoster');
     let $img = $('.movieItem img');
     let $movieInfo = $('.movieItem .movieInfo');
 
-    // $movieItem.on('mouseover',function(index){
-    //     $(this).hover(
-    //             function(){
-    //                 $img.css('opacity',0.4);
-    //                 $movieInfo.css('opacity',1);
-    //             },
-    //             function(){
-    //                 $img.css('opacity',1);
-    //                 $movieInfo.css('opacity',0);
-    //             })
-    // });
-
-    $movieItem.on('mouseover',function(index){
-            $img.css('opacity',0.4);
-            $movieInfo.css('opacity',1);
+    $moviePoster.each(function(index){
+        let img = $img.eq(index);
+        let movieInfo = $movieInfo.eq(index);
+        $(this).on('mouseover',function(){
+            img.css('opacity',0.4);
+            movieInfo.css('opacity',1);
+            $(this).css('box-shadow','0 3px 5 black');
+        })
+        $(this).on('mouseout',function(){
+            img.css('opacity',1);
+            movieInfo.css('opacity',0);
+        })
+        $(this).on('click',function(){
+            $(this).removeClass('marked');
+            $(this).addClass('noMark');
+        })
         });
 
-    $movieItem.on('mouseout',function(index){
-        $img.css('opacity',1);
-        $movieInfo.css('opacity',0);
-    });
-
-    console.log($img);
-    console.log($movieItem);
-    console.log($movieInfo);
 });
 
 
